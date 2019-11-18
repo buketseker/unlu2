f21bb82c3cc974957b42d450512a99f1038c6ae9
Buketseker <buket_skr@hotmail.com>
2019-11-18 09:45:45 +0300

    crm task yüklendi

---
+++ bi_crm_task/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Create Task from Opportunity',
+    'version': '11.0.0.1',
+    'category': 'CRM',
+    'description': """
+    
+""",
+    'author': 'MechSoft',
+    'website': 'http://www.mechsoft.com.tr',
+    'images': [],
+    'depends': ['base', 'crm', 'sale', 'project'],
+    
+    'data': [ 
+             'views/crm_lead_view.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ bi_crm_task/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
+from . import crm_task
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
